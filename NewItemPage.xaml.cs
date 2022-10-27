7f0f75ff665d799b355dfa1e4e64233394728c3f
emmlln <102987339+emmlln@users.noreply.github.com>
2022-10-27 22:36:08 +0300

    Add files via upload

---
+++ NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using PsychoTestApp.Models;
+using PsychoTestApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PsychoTestApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
